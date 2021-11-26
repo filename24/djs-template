8c92c42d9b9ffd58f3a6a38272506ffe1848909e
MadeGOD <anhgerelanhaa87@gmail.com>
2021-11-26 21:54:44 +0800

    feat(manager): added userSend option

--- src/managers/ErrorManager.js
+++ src/managers/ErrorManager.js
@@ -1,11 +1,9 @@
 const BaseManager = require('./BaseManager')
-const Logger = require('@utils/Logger')
+const Embed = require('../utils/Embed')
+const Logger = require('../utils/Logger')
 const uuid = require('uuid')
-/**
- * @type {import('../../config')}
- */
-let config = require('@config')
-const Discord = require('discord.js')
+
+let config = require('../../config')
 
 /**
  * @extends BaseManager
@@ -17,9 +15,7 @@ class ErrorManager extends BaseManager {
    */
   constructor(client) {
     super(client)
-    /**
-     * @type {import('../utils/Logger')}
-     */
+
     this.logger = new Logger('bot')
   }
 
@@ -27,8 +23,9 @@ class ErrorManager extends BaseManager {
    * Report error in Discord Channel
    * @param {Error} error Error object
    * @param {Discord.Message|Discord.CommandInteraction} executer
+   * @param {boolean} [userSend=false] If the error is send to the user
    */
-  report(error, executer) {
+  report(error, executer, userSend = true) {
     this.logger.error(error.stack)
     
 
@@ -38,13 +35,12 @@ class ErrorManager extends BaseManager {
 
     this.client.errors.set(errorCode, error.stack)
 
-    let errorEmbed = new Discord.MessageEmbed()
+    let errorEmbed = new Embed(this.client, 'error')
       .setTitle('오류가 발생했습니다.')
-      .setColor('#ED4245')
       .setDescription('명령어 실행 도중에 오류가 발생하였습니다. 개발자에게 오류코드를 보내 개발에 지원해주세요.')
       .addField('오류 코드', errorCode, true)
 
-    executer.reply({ embeds: [errorEmbed]})
+    userSend ? executer.reply({ embeds: [errorEmbed]}) : null
     if(config.report.type == 'webhook') {
       let webhook = new Discord.WebhookClient({
         url: config.report.webhook.url,
