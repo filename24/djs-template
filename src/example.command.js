dd9935751afbac30ac6488aa865b3a50d23c9cf5
MadeGOD <anhgerelanhaa87@gmail.com>
2021-11-29 05:03:47 +0800

    feat(example): created command file example

--- src/example.command.js
+++ src/example.command.js
@@ -1,14 +1,74 @@
+/* eslint-disable no-unused-vars */
+
+// Slash command and Message Command
+const { SlashCommandBuilder } = require('@discordjs/builders')
 const Discord = require('discord.js')
 
 module.exports = {
   name: '',
   description : '',
   aliases: [],
+  isSlash: false,
   /**
    * @param {import('../../structures/BotClient')} client 
    * @param {Discord.Message} message 
    * @param {string[]} args 
    */
   execute(client, message, args) {
+    
+  },
+  slash: {
+    name: '',
+    data: new SlashCommandBuilder()
+      .setName('')
+      .setDescription('')
+      .toJSON(),
+    /**
+     * 
+     * @param {import('../../structures/BotClient')} client 
+     * @param {Discord.CommandInteraction} interaction 
+     */
+    execute(client, interaction) {
+
+    }
+  }
+}
+
+// Message command
+
+const Discord = require('discord.js')
+
+module.exports = {
+  name: '',
+  description : '',
+  aliases: [],
+  isSlash: false,
+  /**
+   * @param {import('../../structures/BotClient')} client 
+   * @param {Discord.Message} message 
+   * @param {string[]} args 
+   */
+  execute(client, message, args) {
+  },
+}
+
+// Slash command
+const Discord = require('discord.js')
+
+module.exports = {
+  name: '',
+  description : '',
+  isSlash: true,
+  data: new SlashCommandBuilder()
+    .setName('')
+    .setDescription('')
+    .toJSON(),
+  /**
+   * 
+   * @param {import('../../structures/BotClient')} client 
+   * @param {Discord.CommandInteraction} interaction 
+   */
+  execute(client, interaction) {
+
   }
 }
