bb9407fa7a0b733e52c96f18e51d44bcad2e5984
MadeGOD <anhgerelanhaa87@gmail.com>
2021-11-29 05:04:38 +0800

    fix(commands): fixed some errors

--- src/commands/info/ping.js
+++ src/commands/info/ping.js
@@ -15,24 +15,25 @@ module.exports = {
 
     let embed = new Embed(client, 'warn').setTitle('핑 측정중...')
 
-		let m = await message.reply({
-			embeds: [embed],
-		});
-		embed = new Embed(client, 'success')
-			.setTitle('PONG!')
-			.addField('메세지 응답속도', `${m.createdAt - message.createdAt}ms`, true)
-			.addField('API 반응속도', `${client.ws.ping}ms`, true)
+    let m = await message.reply({
+      embeds: [embed],
+    })
+    embed = new Embed(client, 'success')
+      .setTitle('PONG!')
+      .addField('메세지 응답속도', `${m.createdAt - message.createdAt}ms`, true)
+      .addField('API 반응속도', `${client.ws.ping}ms`, true)
       .addField('업타임', `<t:${Number(client.readyAt) / 1000 | 0}:R>`, true)
 
-		m.edit({
-			embeds: [embed],
-		});
+    m.edit({
+      embeds: [embed],
+    })
   },
   slash: {
     name: 'ping',
     data: new SlashCommandBuilder()
       .setName('ping')
-      .setDescription('Checking ping'),
+      .setDescription('핑을 측정합니다.')
+      .toJSON(),
     /**
      * 
      * @param {import('../../structures/BotClient')} client 
