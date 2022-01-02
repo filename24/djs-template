3ba395862e05a1413ad053cb71486ff074227d09
MadeGOD <anhgerelanhaa87@gmail.com>
2022-01-02 17:56:28 +0800

    feat(build): Automated Build data

--- example.config.js
+++ example.config.js
@@ -1,4 +1,20 @@
+const fs = require('fs')
+
+let BUILD_NUMBER = fs.readFileSync('.git/HEAD').toString().trim()
+
+if (BUILD_NUMBER?.indexOf(':') === -1) {
+  BUILD_NUMBER
+} else {
+  try {
+    BUILD_NUMBER = fs.readFileSync('.git/' + BUILD_NUMBER?.substring(5)).toString().trim().substring(0, 6)
+  } catch (e) {
+    BUILD_NUMBER = undefined
+  }
+}
+
 module.exports = {
+  BUILD_VERSION : '0.0.1-dev',
+  BUILD_NUMBER,
   githubToken: '',
   bot: {
     sharding: false,
